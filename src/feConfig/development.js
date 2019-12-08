d86f342594bb11e3ffdcac07f1c524e276aa70bf
lijunchang <lijunchang@iyunxiao.com>
2019-12-08 22:51:00 +0800

    feconfig

--- src/feConfig/development.js
+++ src/feConfig/development.js
@@ -1,3 +1,6 @@
 module.exports = {
-  api: 'https://www.fastmock.site/mock/b0e7c0fd4d0c17abe3d7bda7f9be5104'
+  api: 'https://www.fastmock.site/mock/b0e7c0fd4d0c17abe3d7bda7f9be5104',
+  token: '',
+  appid: '',
+  client_base64: ''
 };
